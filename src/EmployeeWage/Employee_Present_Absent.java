d88fb1fc8f53d35dac2275b935022e6b0a7d6ebf
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-01 17:04:46 +0530

    Employee Present Or Absent

---
+++ src/EmployeeWage/Employee_Present_Absent.java
@@ -0,0 +1,19 @@
+package EmployeeWage;
+
+public class Employee_Present_Absent {
+
+	public static void main(String[] args) 
+    {
+     System.out.println("Welcome to the Employee wage computation program " );    	
+     		// checking attandance
+     		int IS_PRESENT=1;
+		
+		double empCheck=Math.floor(Math.random() * 10 ) %2;
+		if (empCheck==IS_PRESENT){
+			System.out.println("Employee is present");
+		}
+		else{
+			System.out.println("Employee is absent");
+		}
+	}
+}
