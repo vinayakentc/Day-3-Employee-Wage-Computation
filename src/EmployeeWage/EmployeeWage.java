8d1d494ad8cc44b6a3295eb921f061a05090355c
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-01 16:38:09 +0530

    Employee Wage Computation Problem

---
+++ src/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,10 @@
+package EmployeeWage;
+
+//Welcome To Employee Wage Computation Program
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Computation Program");
+	}
+	
+}
